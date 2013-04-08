eac48cc581f7c2de13763f99f0497872255f0833
sanghapal bhowate <sanghapalbhowate89@gmail.com>
2013-04-08 18:01:16 +0530

    add the roles field in user table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User
   include Mongoid::Document
+  include Mongoid::Document::Roleable
+
+  ROLES = ['Admin', 'Student']
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -12,6 +15,7 @@ class User
 
   validates_presence_of :email
   validates_presence_of :encrypted_password
+  attr_accessible :email, :roles, :password, :password_confirmation
   
   ## Recoverable
   field :reset_password_token,   :type => String
