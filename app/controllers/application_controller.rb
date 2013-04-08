f3f3295b82c7362e08e251761ee92e2f62c00397
sanghapal bhowate <sanghapalbhowate89@gmail.com>
2013-04-08 18:41:25 +0530

    modified the application controller file and add the log out link in home page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource && resource.sign_in_count == 1
       root_path
+    else
+      root_path
     end
   end
 
