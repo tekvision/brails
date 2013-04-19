786979379c79b299c9d5e86d35b439819de1ced9
Rupali <kadamrupalidilip@gmail.com>
2013-04-19 17:29:52 +0530

    added rspec validations in models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'fields' do
+      it 'should have description'
+   end
+  end
+
+  context 'required fields' do
+      it 'should have description'
+   end
+  end
+
+  context 'associations' do
+      it 'should belongs to request'
+   end
+  end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'fields' do
+     it 'should have title'
+     it 'should have body'
+     it 'should have status'
+   end
+  end
+
+  context 'required fields' do
+     it 'should have title'
+     it 'should have body'
+     it 'should have status'
+   end
+  end
+
+  context 'associations' do
+     it 'should have many comments'
+   end
+  end
 end
