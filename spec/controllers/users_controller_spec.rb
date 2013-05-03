5bca264026ff4fabe9792a78cfe4203613d3945c
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-05-03 19:34:01 +0530

    Added travis.yml and updated specs so that rake will work silently

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,38 +2,34 @@ require 'spec_helper'
 
 describe UsersController do
 
-context 'role as a Guest' do
-  it 'should see home page after visiting to the site'
-  it 'should invite user'
-end
+  context 'role as a Guest' do
+    it 'should see home page after visiting to the site'
+    it 'should invite user'
+  end
 
 
-context 'role as a Student' do
-   before do
-      it 'should logged in'
-   end
-   it 'should see the list of storyBoard'
-   it 'should choose the storyBoard'
-   it 'should redirected to storyBoardPage after logged in'
-   it 'if Beginner Student' do
-       it 'should starts from basics'
-   end
+  context 'role as a Student' do
+    it 'should see the list of storyBoard'
+    it 'should choose the storyBoard'
+    it 'should redirected to storyBoardPage after logged in'
+    context 'if Beginner Student' do
+      it 'should starts from basics'
+    end
 
-   it 'if Intermediate Student'
-       it 'should starts from anywhere'
-   end
+    it 'if Intermediate Student'
+    it 'should starts from anywhere'
+  end
 
-   it 'if Advance Student' do
-       it 'should skip the basic part and starts form advance concepts'
-   end
-end
+  context 'if Advance Student' do
+    it 'should skip the basic part and starts form advance concepts'
+  end
 
-context 'role as a Admin' do
+  context 'role as a Admin' do
     before do
-       it 'should logged in'
+      it 'should logged in'
     end
 
     it 'should redirected to storyBoardPage after logged in'
     it 'should able to answer the queries'
-   end
+  end
 end
