38958151acffa936baa9cfb07336701c7f024726
sanghapal bhowate <sanghapalbhowate89@gmail.com>
2013-05-17 17:27:42 +0530

    modified the profile factory girl file. add faker method.

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -5,11 +5,11 @@ FactoryGirl.define do
     first_name {Faker::Name.first_name} 
     last_name {Faker::Name.last_name} 
     nick_name "Banti"
-    contact_number "+919881234235"
+    contact_number { Faker::PhoneNumber.phone_number}
     date_of_birth "02/04/1992"
-    country "India"
-    state "Maharashtra"
-    city "Ahamadnagar"
+    city  {Faker::Address.city}
+    state { Faker::Address.state}
+    country { Faker::Address.country}
     permanent_address "sai apartment flat No. 8, shivaji park ahamadnagar 422034"
     local_address "flat No. 3, gokhale park, tingare nagar 411032"
     facebook "tushar.jadhav"
