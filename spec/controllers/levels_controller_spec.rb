2d0ace5c9eec246927c5b59a36f8cba588816b61
Nitin Dhaware <nitin@techvision.net.in>
2013-05-23 18:57:28 +0530

    Have added some test casses in level controller and topic controller

--- spec/controllers/levels_controller_spec.rb
+++ spec/controllers/levels_controller_spec.rb
@@ -9,6 +9,7 @@ describe LevelsController do
   it 'Should display the index' do
     level = create(:level)
     get :index
+    render_template('home#index')
     should respond_with(:success)
     level.should_not be_nil
   end
@@ -19,4 +20,18 @@ describe LevelsController do
     level_cookies = topic.cookies + topic1.cookies
     level_cookies.should_not be_nil
   end
+
+  context "When clicking on the level" do
+    it 'Should show list of topics' do
+      topic = create(:topic)
+      topic1 = create(:topic)
+      topic.title.should_not be_nil
+      topic1.title.should_not be_nil
+    end
+  end
+
+  context "When the level ends" do
+    it 'Should un-lock the bonus round'
+  end
+
 end
