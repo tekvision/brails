0658cb1461c0081140e475176c197c0bbca2ec4d
Nitin Dhaware <nitin@techvision.net.in>
2013-05-28 15:33:06 +0530

    have resolved error of brackets in question controller spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -15,14 +15,16 @@ describe QuestionsController do
 
   it 'Should show questions serially' do
     # There will be a link for each question
-    questions = ["1" => @question, "2" => @question}
+    questions = {"1" => @question, "2" => @question}
     questions["1"].should_not be_nil
     question["2"].should_not be_nil
   end
 
   it 'Should reduce cookies on attempts' do
     question = build(:question)
-    question.options << build(:option, :question => question
+    question.options << build(:option, :question => question)
+    question.options.should false
+
 
   end
 
