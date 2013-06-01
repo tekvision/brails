9f83beffdfd935bb6ff853091f1222974b161afa
sanghapal bhowate <sanghapalbhowate89@gmail.com>
2013-06-01 19:17:44 +0530

    write the rspec form models.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Comment do
 
   context 'fields' do
-    it 'should have description'
+    it {should have_field(:description).of_type(String)}
   end
 
    context 'associations' do
-      it 'should belongs to request'
+      it {should belong_to(:request)}
    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,31 @@ require 'spec_helper'
 describe User do
 
   context 'fields' do
-     it 'should have email'
-     it 'should have password'
-     it 'should have role'
+     it { should have_field(:email).of_type(String)}
+     it { should have_field(:encrypted_password).of_type(String)}
+     it { should have_field(:roles).of_type(String)}
+  end
+
+  context 'Mass assignment' do
+    it { should allow_mass_assignment_of(:email) }
+    it { should allow_mass_assignment_of(:roles) }
+    it { should allow_mass_assignment_of(:password) }
+    it { should allow_mass_assignment_of(:password_confirmation) }
   end
 
   context 'Required fields' do
-    it 'should have email'
-    it 'should have password'
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:password)}
+    it { should validate_presence_of(:roles)}
+  end
+
+  context 'Validations' do
+    it { should validate_uniqueness_of(:email).case_insensitive.with_message("is already taken") }
   end
 
   context 'Associations' do
-    it 'Should embed one profile'
-    it 'should have many requests'
+    it { should embed_one :profile }
+    it { should have_many(:requests)}
+    it { should have_many(:questions)}
   end
 end
