2acddd27fdb28ee67f1a6fcfec5cfcafd93841c3
Nitin Dhaware <nitin@techvision.net.in>
2013-06-03 14:23:01 +0530

    Have removed user_cookie model. Have murged the functionality with user_topic model.

--- app/models/user_cookie.rb
+++
@@ -1,7 +0,0 @@
-class UserCookie
-  include Mongoid::Document
-  field :topic_cookies, type: Integer
-
-  belongs_to :user
-  belongs_to :topic
-end
--- app/models/user_topic.rb
+++ app/models/user_topic.rb
@@ -1,6 +1,7 @@
 class UserTopic
   include Mongoid::Document
   field :is_completed, type: Boolean, default: false
+  field :topic_cookies, type: Integer
 
   belongs_to :user
   belongs_to :topic
