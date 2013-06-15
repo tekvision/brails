3e00f3a5f8c3a0896e91771cf90b91af35a3a677
Nitin Dhaware <nitin@techvision.net.in>
2013-06-15 10:53:44 +0530

    Have uncomented authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
   #TODO: uncomment during development
-	#before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def after_sign_in_path_for(resource)
     if resource && resource.sign_in_count == 1
