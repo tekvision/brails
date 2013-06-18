55b9a0a78ad91800c298373bdc33d9146f64f150
Nitin Dhaware <nitin@techvision.net.in>
2013-06-18 19:38:19 +0530

    Have changed the logic of showing cookies in levels/show. Have added a method in application controller to calculate cookies for a topic.

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -3,8 +3,9 @@ class Attempt
 
   field :count, type: Integer, default: 0
   field :solved, type: Boolean, default: false
-  field :cookies, type: Integer
+  field :cookies, type: Integer, default: 0
 
   belongs_to :user
   belongs_to :question
+  belongs_to :topic
 end
