d37a6d8623f50feb7b760dccdf18372075aee1d7
SpiderGears <deepakkumar0511@gmail.com>
2013-07-03 14:45:42 +0530

    local commit

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -14,6 +14,8 @@ RailsAdmin.config do |config|
   # RailsAdmin may need a way to know who the current user is]
   config.current_user_method { current_user } # auto-generated
 
+  config.authorize_with :cancan
+
   # If you want to track changes on your models:
   # config.audit_with :history, 'User'
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe UsersController do
   context "When user is not logged-in" do
-    it 'Should redirect to home page'
-      Should redirect_to home_path
+    it 'Should redirect to home page' do
+      response.should redirect_to '/users/login' 
     end
   end
 
@@ -12,27 +12,26 @@ describe UsersController do
     it 'should invite user'
   end
 
-context 'role as a Student' do
-   before do
-      it 'should register'
-      it 'should logged in'
-   end
-   it 'should redirected to storyBoardPage after logged in'
-   it 'should see the list of storyBoard'
-   it 'should choose the storyBoard'
-   it 'if Beginner student' do
-       it 'should starts from basics'
-       it 'should redirect to basic storyboard after clicking on it'
-   end
-   it 'if an Advanced student' do
-       it 'should skip the basic part and starts from advance concepts'
-       it 'should redirect to advance storyboard after clicking on it'
-   end
-end
+  context 'role as a Student' do
+    before do
+     # it 'should register'
+     # it 'should logged in'
+    end
+    it 'should redirected to storyBoardPage after logged in'
+    it 'should see the list of storyBoard'
+    it 'should choose the storyBoard'
+    context 'if Beginner student' do
+      it 'should starts from basics'
+      it 'should redirect to basic storyboard after clicking on it'
+    end
+    context 'if an Advanced student' do
+      it 'should skip the basic part and starts from advance concepts'
+      it 'should redirect to advance storyboard after clicking on it'
+    end
+  end
 
     it 'if Intermediate Student'
     it 'should starts from anywhere'
-  end
 
   context 'if Advance Student' do
     it 'should skip the basic part and starts form advance concepts'
@@ -40,13 +39,12 @@ end
 
   context 'role as a Admin' do
     before do
-       it 'should register as an admin'
-       it 'should logged in'
+      # it 'should register as an admin'
+      # it 'should logged in'
     end
     it 'should redirected to storyBoardPage after logged in'
     it 'should manage administration role & provide support'
     it 'should redirected to storyBoardPage after logged in'
     it 'should able to answer the queries'
   end
-
-end
+end 
