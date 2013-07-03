a08b7595c65c822d87ea8dfa517ff8ef00ad8d66
SpiderGears <deepakkumar0511@gmail.com>
2013-07-03 17:34:51 +0530

    Added env/devlopment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Brails::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {   
   :address => "smtp.gmail.com",   
   :port => 587,   
