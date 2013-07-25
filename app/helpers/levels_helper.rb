72e995af2ad91472e32adafc5461ff3304462c24
SpiderGears <deepakkumar0511@gmail.com>
2013-07-25 17:15:33 +0530

    Show of coins for Bonus Round

--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -1,2 +1,17 @@
 module LevelsHelper
-end
+
+  def calculate_usercoins_bonusround(bonus_round)
+    bonus_round.questions.inject(0) do |count, question|
+      count + question.attempts.where(user_id: current_user.id).inject(0)do |count, attempt|
+        count + attempt.coins
+      end
+    end
+  end
+  
+  def calculate_totalcoins_bonusround(bonus_round)
+    bonus_round.questions.inject(0) do |count, question|
+      count + H_COOKIES[question.question_type] 
+    end
+  end
+
+end 
